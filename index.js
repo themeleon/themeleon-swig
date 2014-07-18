e31e4631af4df57d181ed5134ef305950b3e1772
Valérian Galliat <vava720@gmail.com>
2014-07-18 22:44:35 +0200

    Anonymous function.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var swig = require('swig');
 var renderFile = q.denodeify(swig.renderFile);
 var writeFile = q.denodeify(fs.writeFile);
 
-exports.swig = d.srcDest(function swig(src, dest) {
+exports.swig = d.srcDest(function (src, dest) {
   var writeIndex = ap([dest], writeFile);
   this.push(renderFile(src, this.ctx).then(writeIndex));
 });
