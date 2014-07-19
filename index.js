da296c25b7d345d4895e404b3116dcbc4586c169
Valérian Galliat <vava720@gmail.com>
2014-07-19 11:52:49 +0200

    Allow to inject Swig instance.

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ var ap = require('ap');
 var d = require('themeleon/decorators');
 var fs = require('fs');
 var q = require('q');
-var swig = require('swig');
 
-var renderFile = q.denodeify(swig.renderFile);
-var writeFile = q.denodeify(fs.writeFile);
+module.exports = function (swig) {
+  swig = swig || require('swig');
 
-exports.swig = d.srcDest(function (src, dest) {
-  var writeIndex = ap([dest], writeFile);
-  this.push(renderFile(src, this.ctx).then(writeIndex));
-});
+  var renderFile = q.denodeify(swig.renderFile);
+  var writeFile = q.denodeify(fs.writeFile);
+
+  return {
+    swig: d.srcDest(function (src, dest) {
+      var writeIndex = ap([dest], writeFile);
+      this.push(renderFile(src, this.ctx).then(writeIndex));
+    });
+  };
+};
